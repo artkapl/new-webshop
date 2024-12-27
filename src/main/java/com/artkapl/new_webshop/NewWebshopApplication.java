cc27b7e8c414cdc40eaef88130048630833fb368
artkapl <arthur.kaplan@gmx.at>
2024-12-27 15:23:39 +0100

    initial commit

---
+++ src/main/java/com/artkapl/new_webshop/NewWebshopApplication.java
@@ -0,0 +1,13 @@
+package com.artkapl.new_webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewWebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewWebshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artkapl/new_webshop/NewWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artkapl.new_webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewWebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
