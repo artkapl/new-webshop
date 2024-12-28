aefa861292a43ec8054f84e0b82b4b4eeb3625cf
artkapl <arthur.kaplan@gmx.at>
2024-12-28 21:25:29 +0100

    Category name must be unique

--- src/main/java/com/artkapl/new_webshop/model/Category.java
+++ src/main/java/com/artkapl/new_webshop/model/Category.java
@@ -2,12 +2,12 @@ package com.artkapl.new_webshop.model;
 
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,7 +23,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
+    @Column(nullable = false, unique = true)
     private String name;
 
     private String description;
