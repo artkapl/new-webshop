d847e226ecd3ab1e1f92abf2896f93ea6f5459ed
artkapl <arthur.kaplan@gmx.at>
2024-12-28 21:25:07 +0100

    update exceptions

--- src/main/java/com/artkapl/new_webshop/exception/ProductNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.artkapl.new_webshop.exception;
-
-public class ProductNotFoundException extends RuntimeException {
-
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-
-}
