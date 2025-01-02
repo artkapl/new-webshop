b68d85eed70c17501f87abfa3556a0e5e33b7b93
artkapl <arthurkaplan@gmx.at>
2025-01-02 20:36:59 +0100

    add category controller

--- src/main/java/com/artkapl/new_webshop/controller/CategoryController.java
+++ src/main/java/com/artkapl/new_webshop/controller/CategoryController.java
@@ -1,5 +1,8 @@
 package com.artkapl.new_webshop.controller;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 import java.util.List;
@@ -9,10 +12,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.artkapl.new_webshop.exception.AlreadyExistsException;
 import com.artkapl.new_webshop.model.Category;
 import com.artkapl.new_webshop.service.CategoryService;
 import com.artkapl.response.ApiResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -29,9 +36,20 @@ public class CategoryController {
             String message = categories.isEmpty() ? "No categories found!" : "Categories found!";
             return ResponseEntity.ok(new ApiResponse(message, categories));
         } catch (Exception e) {
-            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Something went wrong!", INTERNAL_SERVER_ERROR));
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Something went wrong!" + e.getMessage(), INTERNAL_SERVER_ERROR));
         }
-        
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse> addCategory(@RequestBody Category category) {
+        try {
+            Category newCategory = categoryService.createCategory(category);
+            return ResponseEntity.status(CREATED).body(new ApiResponse("Category added!", newCategory));    
+        } catch (AlreadyExistsException e) {
+            return ResponseEntity.status(CONFLICT).body(new ApiResponse(e.getMessage(), null));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Something went wrong!" + e.getMessage(), INTERNAL_SERVER_ERROR));
+        }
     }
 
 }
