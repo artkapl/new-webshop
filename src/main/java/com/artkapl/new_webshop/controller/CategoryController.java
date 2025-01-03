68a302de5932c10f066807f597cc8937584918f4
artkapl <arthurkaplan@gmx.at>
2025-01-03 13:29:41 +0100

    update category controller

--- src/main/java/com/artkapl/new_webshop/controller/CategoryController.java
+++ src/main/java/com/artkapl/new_webshop/controller/CategoryController.java
@@ -16,10 +16,14 @@ import com.artkapl.new_webshop.exception.NotFoundException;
 import com.artkapl.new_webshop.model.Category;
 import com.artkapl.new_webshop.service.CategoryService;
 import com.artkapl.response.ApiResponse;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -54,7 +58,7 @@ public class CategoryController {
         }
     }
 
-    @GetMapping("/{name}")
+    @GetMapping("/category/{name}")
     public ResponseEntity<ApiResponse> getCategoryByName(@PathVariable String name) {
         try {
             Category category = categoryService.getCategoryByName(name);
@@ -74,6 +78,30 @@ public class CategoryController {
             return ResponseEntity.status(CREATED).body(new ApiResponse("Category added!", newCategory));    
         } catch (AlreadyExistsException e) {
             return ResponseEntity.status(CONFLICT).body(new ApiResponse(e.getMessage(), null));
+        } catch (Exception e) {
+            return ControllerTools.getInternalErrorResponse(e);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse> deleteCategory(@PathVariable Long id) {
+        try {
+            categoryService.deleteCategory(id);
+            return ResponseEntity.ok(new ApiResponse("Category deleted!", null));
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), NOT_FOUND));
+        } catch (Exception e) {
+            return ControllerTools.getInternalErrorResponse(e);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse> updateCategory(@PathVariable Long id, @RequestBody Category category) {
+        try {
+            Category updatedCategory = categoryService.updateCategory(category, id);
+            return ResponseEntity.ok(new ApiResponse("Category updated!", updatedCategory));
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), NOT_FOUND));
         } catch (Exception e) {
             return ControllerTools.getInternalErrorResponse(e);
         }
