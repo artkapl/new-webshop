97751a4c08757a3caa6c984fba021f6a53e9b729
artkapl <arthurkaplan@gmx.at>
2025-01-05 18:04:08 +0100

    do not store uploaded images in DB

--- src/main/java/com/artkapl/new_webshop/model/Image.java
+++ src/main/java/com/artkapl/new_webshop/model/Image.java
@@ -1,13 +1,10 @@
 package com.artkapl.new_webshop.model;
 
-import java.sql.Blob;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -30,9 +27,6 @@ public class Image {
 
     @NotBlank
     private String fileType;
-
-    // @Lob
-    // private Blob imageData;
 
     private String imageUrl;
 
